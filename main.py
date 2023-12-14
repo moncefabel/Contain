dc6e9b16f02e210a4c9a924a07d441b464d45049
Bakir YAGOUB <88115043+Jacoub-Bakir@users.noreply.github.com>
2023-12-14 17:25:07 +0100

    Update main.py

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ def classifier(mat, model):
         pred : list of predicted labels
     '''
     if model == 'SVM':
-        model = pickle.load(open("classification_model.pkl", "rb"))
+        model = pickle.load(open("svm_model.pkl", "rb"))
         pred = model.predict(mat)
 
     elif model == 'RF':
